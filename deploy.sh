d8fc868cfde89141025544fa15104ca3787869da
Raymond Hammarling <raymond@hammarlings.se>
2018-02-12 19:43:30 +0100

    Add CHECKS file

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ export $(egrep -v '^#' .env.deploy | xargs)
 echo "Deploying to app '$DOKKU_APP' at $DOKKU_HOST"
 
 npm run build
-tar -c dist/ Dockerfile | ssh "dokku@$DOKKU_HOST" tar:in "$DOKKU_APP"
+tar -c dist/ Dockerfile CHECKS | ssh "dokku@$DOKKU_HOST" tar:in "$DOKKU_APP"
