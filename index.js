6225ad173d431d06b8cc0d90b06edcb81b638f59
Raymond Hammarling <raymond@hammarlings.se>
2018-02-23 05:34:42 +0100

    Move main converter function to separate file

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var css = require('sheetify')
 var choo = require('choo')
-const converterGba = require('./converter-gba')
-const converterDsGen4 = require('./converter-ds-gen4')
-
-function convert(file, showdownPokedexData) {
-  return converterDsGen4.convert(file) || { output: converterGba.convert(file, { showdownPokedexData }) }
-}
+const convert = require('./converter')
 
 css('tachyons')
 
