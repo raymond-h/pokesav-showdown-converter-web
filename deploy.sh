efe2956c9b37a3cd4ed0659c40979313fd9ee63a
Raymond Hammarling <raymond@hammarlings.se>
2022-06-03 15:48:27 +0200

    Clean up old deployment stuff

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# read vars from .env.deploy file
-export $(egrep -v '^#' .env.deploy | xargs)
-
-echo "Deploying to app '$DOKKU_APP' at $DOKKU_HOST"
-
-npm run build
-tar -c dist/ Dockerfile CHECKS | ssh "dokku@$DOKKU_HOST" tar:in "$DOKKU_APP"
